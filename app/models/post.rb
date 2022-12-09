b5e9efbee716e2234e3b92e9fbbcef238d6acb6b
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-09 15:59:05 +0300

    Add validation for user

--- app/models/post.rb
+++ app/models/post.rb
@@ -14,4 +14,9 @@ class Post < ApplicationRecord
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
+
+  validates :title, presence: true, length: { maximum: 250, too_long: '250 characters in post is the maximum allowed.' }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
 end
