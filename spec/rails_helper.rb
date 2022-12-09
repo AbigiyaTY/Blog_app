dc39e358daa1e7e7e50f4318c7dec77fa805164d
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-09 21:03:48 +0300

    Fix linter

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -24,6 +24,7 @@ require 'rspec/rails'
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
+# rubocop:disable all
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
