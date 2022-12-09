6508ebe8a44f7a864171196501454b5566301213
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-09 20:46:22 +0300

    Add test for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,18 +5,19 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
-  private
+  validates :title, presence: true, length: { maximum: 250, too_long: '250 characters in post is the maximum allowed.' }
+  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
-  def update_post_counter
-    post.increment!(:posts_counter)
-  end
 
   def recent_comments
     comments.order(created_at: :desc).limit(5)
   end
 
-  validates :title, presence: true, length: { maximum: 250, too_long: '250 characters in post is the maximum allowed.' }
-  validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
+  end
 
 end
