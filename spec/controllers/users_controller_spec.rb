0af8eb59bda2d5d5ecec06a275a4cf59c93a375b
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-14 02:18:22 +0300

    Add test for user

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :request do
+    describe 'GET #index' do
+        it 'returns a success response' do
+        get users_url
+        expect(response).to be_successful
+        end
+    end
+
+    describe 'users#show' do
+        it 'show for users works!' do
+            get '/users/1'
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include('Teacher from Mexico.')
+        end
+    end
+
+end
