99dc9a6cc4d90bb786ef5f8f80d873ed68c5d296
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-14 01:42:10 +0300

    Add test for post

---
+++ spec/controllers/posts_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Posts', type: :request do
+    describe 'GET /index' do
+        it 'works! (now write some real specs)' do
+            get posts_path
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include('Posts')
+        end
+    end
+
+    describe 'posts#show' do
+        it 'works! (now write some real specs)' do
+            get '/users/1/posts/1'
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include('This is my first post')
+        end
+    end
+
+
+end
