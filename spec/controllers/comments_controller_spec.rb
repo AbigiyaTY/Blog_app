99f392c33cefcc61420f7558e2b1e71f69653415
Abigiya Tadesse <Abigiyatadesse23@gmail.com>
2022-12-14 01:42:43 +0300

    Update test for comment

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,41 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe CommentsController, type: :controller do
-    let(:comment) { Comment.create(author_id: 1, post_id: 1, text: 'This is a comment') }
-
-        it 'It should response status was correct' do
-            get :index
-            expect(response).to have_http_status(:success)
+RSpec.describe CommentsController, type: :request do
+    describe 'GET /index' do
+        it 'works! (now write some real specs)' do
+            get comments_path
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include('Comments')
         end
+    end
 
-        it 'It should response status was correct' do
-            get :show, params: { id: comment.id }
-            expect(response).to have_http_status(:success)
+    describe 'comments#show' do
+        it 'works! (now write some real specs)' do
+            get '/users/1/posts/1/comments/1'
+            expect(response).to have_http_status(:ok)
+            expect(response.body).to include('Hi Tom!')
         end
-
-        it 'It should response status was correct' do
-            get :new
-            expect(response).to have_http_status(:success)
-        end
-
-        it 'It should response status was correct' do
-            get :edit, params: { id: comment.id }
-            expect(response).to have_http_status(:success)
-        end
-
-        it 'It should response status was correct' do
-            post :create, params: { comment: { author_id: 1, post_id: 1, text: 'This is a comment' } }
-            expect(response).to have_http_status(:found)
-        end
-
-        it 'It should response status was correct' do
-            put :update, params: { id: comment.id, comment: { author_id: 1, post_id: 1, text: 'This is a comment' } }
-            expect(response).to have_http_status(:found)
-        end
-
-        it 'It should response status was correct' do
-            delete :destroy, params: { id: comment.id }
-            expect(response).to have_http_status(:found)
-        end
+    end
 
 end
